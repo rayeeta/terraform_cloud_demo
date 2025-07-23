087f434c70fe01f3f61758320a8b954887bd84e4
Ray <efarayeta@gmail.com>
2025-07-23 10:31:22 +0200

    Fix

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
-   provider "aws" {
-     region = var.region
-   }
+provider "aws" {
+  region = "us-east-1"  # Change to your desired region
+}
 
-   resource "aws_instance" "web_server" {
-     ami           = var.ami_id
-     instance_type = var.instance_type
-     tags = {
-       Name = "TF-Cloud-Demo"
-     }
-   }
-   
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 (BIOS boot mode, us-east-1)
+  instance_type = "t2.micro"               # Free-tier eligible
+
+  tags = {
+    Name = "Terraform_Cloud_WebServer"
+  }
+}
+
--- outputs.tf
+++
@@ -1,8 +0,0 @@
-   output "instance_id" {
-     value = aws_instance.web_server.id
-   }
-
-   output "public_ip" {
-     value = aws_instance.web_server.public_ip
-   }
-   
