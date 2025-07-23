e22cb56dbe8816f950ba441fb6ddbfe442464c6f
Ray <efarayeta@gmail.com>
2025-07-23 10:52:51 +0200

    fix s3

--- s3.tf
+++ s3.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "rayeeta_Belgium"  # Must be globally unique
+  bucket = "rayeetaBelgium"  # Must be globally unique
   acl    = "private"
 
   tags = {
-    Name        = "Demo_bucket"
+    Name        = "DemoBucket"
     Environment = "Dev"
   }
 }
